05491a77de1b5e996842c51ab15d9a26b24e7748
Anshul Gupta <anshul.ag6023@gmail.com>
2024-07-28 21:34:47 +0530

    Add files via upload

--- demo.java
+++ demo.java
@@ -12,32 +12,32 @@ public class demo {
 	private static final String url = "jdbc:oracle:thin:@localhost:1521:orcl";
 	private static final String username = "c##anshul";
 	private static final String password = "anshul";
-	private static final String AddEmployee = "INSERT INTO EMPLOYEE VALUES (?,?,?,?,?)";
-	private static final String AllEmployee = "SELECT * FROM EMPLOYEE";
-	private static final String getByEmployeeId = "SELECT * FROM EMPLOYEE WHERE EMPLOYEE_ID = ?";
-	private static final String updateEmployeeBsal = "UPDATE EMPLOYEE SET BSAL= ?, TOTSAL= ? WHERE EMPLOYEE_ID= ?";
-	private static final String deleteEmployee = "DELETE FROM EMPLOYEE WHERE EMPLOYEE_ID= ?";
+	private static final String AddBookDetails = "INSERT INTO BOOKDETAILS VALUES (?,?,?,?,?)";
+	private static final String AllBookDetails = "SELECT * FROM BOOKDETAILS";
+	private static final String getBookbyCode = "SELECT * FROM BOOKDETAILS WHERE CODE = ?";
+	private static final String updateBookDetails = "UPDATE BOOKDETAILS SET PRICE = ?, QTY = ? WHERE CODE = ?";
+	private static final String deleteBook = "DELETE FROM BOOKDETAILS WHERE CODE= ?";
 	
 	public static void main(String[] args) throws Exception{
 		
 		Class.forName(driver_ClassName);
 		Connection con = DriverManager.getConnection(url, username, password);
 		
-		PreparedStatement ps1 = con.prepareStatement(AddEmployee);
-		PreparedStatement ps2 = con.prepareStatement(AllEmployee);
-		PreparedStatement ps3 = con.prepareStatement(getByEmployeeId);
-		PreparedStatement ps4 = con.prepareStatement(updateEmployeeBsal);
-		PreparedStatement ps5 = con.prepareStatement(deleteEmployee);
+		PreparedStatement ps1 = con.prepareStatement(AddBookDetails);
+		PreparedStatement ps2 = con.prepareStatement(AllBookDetails);
+		PreparedStatement ps3 = con.prepareStatement(getBookbyCode);
+		PreparedStatement ps4 = con.prepareStatement(updateBookDetails);
+		PreparedStatement ps5 = con.prepareStatement(deleteBook);
 		
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
 		
 		while(true) {
-			System.out.println("\n\n1. Add Employee.");
-			System.out.println("2. See All Employee.");
-			System.out.println("3. See All Employee by ID.");
-			System.out.println("4. Update Employee Bsal by ID.");
-			System.out.println("5. Delete Record by ID.");
+			System.out.println("\n\n1. Add Book Details.");
+			System.out.println("2. See All Books.");
+			System.out.println("3. See All Book Details by Code.");
+			System.out.println("4. Update Book Price/QTY by Code.");
+			System.out.println("5. Delete Book Detail by Code.");
 			System.out.println("6. Exit.\n\n");
 			
 			System.out.println("Enter Your Choice : ");
@@ -45,28 +45,26 @@ public class demo {
 			
 			switch(opt) {
 			case "1":{
-				System.out.println("Enter your Employee_ID: ");
-				int id = Integer.parseInt(br.readLine());
+				System.out.println("Enter Book Code: ");
+				int code = Integer.parseInt(br.readLine());
 				
-				System.out.println("Enter your NAME: ");
+				System.out.println("Enter Book Name: ");
 				String name = br.readLine();
 				
-				System.out.println("Enter your DESG: ");
-				String desg = br.readLine();
+				System.out.println("Enter Author Name: ");
+				String author = br.readLine();
 				
-				System.out.println("Enter your BSAL: ");
-				int bsal = Integer.parseInt(br.readLine());
+				System.out.println("Enter Book Price: ");
+				int price = Integer.parseInt(br.readLine());
 				
-				double hra = 0.93 * bsal;
-                double da = 0.61 * bsal;
-                double totsal = bsal + hra + da;
+				System.out.println("Enter Book QTY: ");
+				int qty = Integer.parseInt(br.readLine());
 				
-				
-				ps1.setInt(1, id);
-				ps1.setString(2, name);
-				ps1.setString(3, desg);
-				ps1.setDouble(4, bsal);
-				ps1.setDouble(5, totsal);
+				ps1.setInt(1, code);
+		        ps1.setString(2, name);
+		        ps1.setString(3, author);
+		        ps1.setInt(4, price);
+		        ps1.setInt(5, qty);
 				
 				int k = ps1.executeUpdate();
 				if (k > 0)
@@ -77,19 +75,18 @@ public class demo {
 				break;
 				
 			case "2":{
-				ResultSet rs = ps2.executeQuery();
+				ResultSet rs2 = ps2.executeQuery();
 				
-				while(rs.next()) {
-					System.out.println(rs.getInt(1)+ "\t" + rs.getString(2)+ "\t" + rs.getString(3)+ "\t" + rs.getInt(4)+ "\t" + rs.getInt(5));
+				while(rs2.next()) {
+					System.out.println(rs2.getInt(1)+ "\t" + rs2.getString(2)+ "\t" + rs2.getString(3)+ "\t" + rs2.getInt(4)+ "\t" + rs2.getInt(5));
 				}
 			}
 				break;
 				
 			case "3":{
-			
-				System.out.println("Enter Employee_Id: ");
-				int id = Integer.parseInt(br.readLine());
-				ps3.setInt(1, id);
+				System.out.println("Enter Book Code: ");
+				int code = Integer.parseInt(br.readLine());
+				ps3.setInt(1, code);
 				
 				ResultSet rs = ps3.executeQuery();
 				if(rs.next()) {
@@ -101,43 +98,33 @@ public class demo {
 				break;
 				
 			case "4":{
+				System.out.println("Enter Book Code: ");
+				int code = Integer.parseInt(br.readLine());
 				
-				 System.out.println("Enter Employee Id: ");
-                 int id = Integer.parseInt(br.readLine());
-                 ps3.setInt(1, id);
-                 ResultSet rs = ps3.executeQuery();
-                 if (rs.next()) {
-                     double oldBsal = rs.getDouble("BSAL");
-                     System.out.println("OldBsal: " + oldBsal);
-                     System.out.println("OldtotSal: " + rs.getDouble("TOTSAL"));
-                     
-                     System.out.println("Enter new BSAL: ");
-                     double newBsal = Double.parseDouble(br.readLine());
-                     
-                     double hra = 0.93 * newBsal;
-                     double da = 0.61 * newBsal;
-                     double totSal = newBsal + hra + da;
-                     
-                     ps4.setDouble(1, newBsal);
-                     ps4.setDouble(2, totSal);
-                     ps4.setInt(3, id);
-                     
-                     int k = ps4.executeUpdate();
-                     if (k > 0) {
-                         System.out.println("Record Updated");
-                     } else {
-                         System.err.println("Failed to update.");
-                     }
-                 } else {
-                     System.err.println("Invalid Employee Id.");
-                 }
-             }
-             break;
+				System.out.println("Enter New Price: ");
+				int price = Integer.parseInt(br.readLine());
+				
+				System.out.println("Enter New Qty: ");
+				int qty = Integer.parseInt(br.readLine());
+				
+				 ps4.setInt(1, price);
+			     ps4.setInt(2, qty);
+			     ps4.setInt(3, code);
+
+			     int result = ps4.executeUpdate();
+			     if (result > 0) {
+			    	 System.out.println("Book details updated successfully.");
+			     }
+			     else {
+			            System.out.println("Failed to update book details.");
+			        }
+			}
+				break;
+				
 			case "5":{
-				
-				System.out.println("Enter Employee Id: ");
-                int id = Integer.parseInt(br.readLine());
-                ps5.setInt(1, id);
+				System.out.println("Enter Book Code: ");
+                int code = Integer.parseInt(br.readLine());
+                ps5.setInt(1, code);
                 
                 int k = ps5.executeUpdate();
                 if (k > 0) {
@@ -147,6 +134,7 @@ public class demo {
                 }
 			}
 				break;
+				
 			case "6":{
 				System.out.println("Good Bye");
 				System.exit(0);
@@ -156,5 +144,5 @@ public class demo {
 				System.out.println("INVALID OPTION SELECTED.");
 			}
 		}
-	}
+	}	
 }
